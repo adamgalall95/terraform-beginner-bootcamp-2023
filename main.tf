095bf95de76b3af3544af7e9089ccd7dbeb99169
adam galall <ahmedgalall95@outlook.com>
2023-09-26 06:33:45 +0000

    terraform basics

--- main.tf
+++ main.tf
@@ -1,2 +1,26 @@
-# this is my first changw!
-#this is my second change
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
+
+provider "random" {
+  # Configuration options
+}
+
+resource "random_string" "bucket_name" {
+  length           = 16
+  special          = false
+  override_special = ""
+}
+
+output "random_bucket_name_id" {
+    value = random_string.bucket_name.id
+}
+
+output "random_bucket_name_result" {
+    value = random_string.bucket_name.result
+}
