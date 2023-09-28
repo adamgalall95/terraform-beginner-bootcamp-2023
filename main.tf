0f22a7d55a7d193478fa9d18c0628dbe9db6c7d6
adam galall <ahmedgalall95@outlook.com>
2023-09-28 06:01:06 +0000

    #tf changes

--- main.tf
+++ main.tf
@@ -1,28 +1,3 @@
-terraform {
-  cloud {
-    organization = "terraform-bootcamp-2023-updated"
-
-    workspaces {
-      name = "terra-house-galall-1"
-    }
-  }
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
-}
-
-
-provider "random" {
-  # Configuration options
-}
-
 resource "random_string" "bucket_name" {
   lower = true
   upper = false
@@ -33,12 +8,8 @@ resource "random_string" "bucket_name" {
 
 resource "aws_s3_bucket" "bucket_name" {
   bucket = random_string.bucket_name.result
-}
 
-output "random_bucket_name_id" {
-    value = random_string.bucket_name.id
-}
-
-output "random_bucket_name_result" {
-    value = random_string.bucket_name.result
+  tags = {
+    UserUuid = var.user_uuid
+  }
 }
