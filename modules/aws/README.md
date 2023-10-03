d5530ff3d51cea84024e004ebdb723c4c396d0b6
adam galall <ahmedgalall95@outlook.com>
2023-10-03 09:14:08 +0000

    #28 index module

---
+++ modules/aws/README.md
@@ -0,0 +1,3 @@
+1- You need to declare resources in main.tf/modules and call them in main.tf/main
+2- You need to declare input variables in variables/modules and call them in variables/main
+3- You need to declare outputs in outputs/modules and call them in outputs/main
--- modules/aws/main.tf
+++ modules/aws/main.tf
@@ -16,5 +16,21 @@ resource "aws_s3_bucket" "website_bucket" {
   }
 }
 
+resource "aws_s3_bucket_website_configuration" "web_config" {
+  bucket = aws_s3_bucket.website_bucket.bucket
 
+  index_document {
+    suffix = "index.html"
+  }
 
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_object" "index" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key    = "index.html"
+  source = "/workspace/terraform-beginner-bootcamp-2023/public/index.html"
+  etag = filemd5("/workspace/terraform-beginner-bootcamp-2023/public/index.html")
+}
