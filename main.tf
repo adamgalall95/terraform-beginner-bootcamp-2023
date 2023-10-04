d79b4e44fedc3846d4f53ce210d9a4fbf50d5958
adamgalall95 <145528713+adamgalall95@users.noreply.github.com>
2023-10-04 15:20:47 +1030

    28 s3 static website hosting (#29)
    * #28 index module
    
    * #28 enabled static website hosting, uploaded index and error via tf

--- main.tf
+++ main.tf
@@ -34,4 +34,6 @@ module "terrahouse_aws" {
   source = "./modules/aws"
   user_uuid = var.user_uuid
   bucket_name = var.bucket_name
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
 }
