eab7defc92f5c0854ce3ef29c79335e3168c4da8
adam galall <ahmedgalall95@outlook.com>
2023-10-05 07:22:18 +0000

    #34 invalidate cache

--- modules/aws/outputs.tf
+++ modules/aws/outputs.tf
@@ -5,3 +5,7 @@ output "bucket_name" {
 output "website_endpoint" {
     value = aws_s3_bucket_website_configuration.web_config.website_endpoint
 }
+
+output "cloudfront_url" {
+    value = aws_cloudfront_distribution.default.domain_name
+}
